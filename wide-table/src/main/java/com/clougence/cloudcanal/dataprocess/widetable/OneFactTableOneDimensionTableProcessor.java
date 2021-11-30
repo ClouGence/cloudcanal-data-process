2b7ebdd389eb437ec48ab6399d42111beacb4f58
zylicfc <zylicfc@192.168.0.171>
2021-11-30 18:06:51 +0800

    fix(ds):fix logic

--- wide-table/src/main/java/com/clougence/cloudcanal/dataprocess/widetable/OneFactTableOneDimensionTableProcessor.java
+++ wide-table/src/main/java/com/clougence/cloudcanal/dataprocess/widetable/OneFactTableOneDimensionTableProcessor.java
@@ -1,20 +1,24 @@
 package com.clougence.cloudcanal.dataprocess.widetable;
 
-import com.clougence.cloudcanal.sdk.api.CloudCanalProcessor;
-import com.clougence.cloudcanal.sdk.api.contextkey.RdbContextKey;
-import com.clougence.cloudcanal.sdk.api.metakey.RdbMetaKey;
-import com.clougence.cloudcanal.sdk.api.model.CustomField;
-import com.clougence.cloudcanal.sdk.api.model.CustomProcessorContext;
-import com.clougence.cloudcanal.sdk.api.model.CustomRecord;
-import com.clougence.cloudcanal.sdk.api.service.impl.RecordBuilder;
-
-import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
+
+import com.clougence.cloudcanal.sdk.api.CloudCanalProcessor;
+import com.clougence.cloudcanal.sdk.api.constant.rdb.RecordAction;
+import com.clougence.cloudcanal.sdk.api.contextkey.RdbContextKey;
+import com.clougence.cloudcanal.sdk.api.metakey.RdbMetaKey;
+import com.clougence.cloudcanal.sdk.api.model.CustomField;
+import com.clougence.cloudcanal.sdk.api.model.CustomProcessorContext;
+import com.clougence.cloudcanal.sdk.api.model.CustomRecord;
+import com.clougence.cloudcanal.sdk.api.service.impl.RecordBuilder;
 
 /**
  * @author bucketli 2021/11/29 23:07:26
@@ -28,19 +32,23 @@ public class OneFactTableOneDimensionTableProcessor implements CloudCanalProcess
             for (CustomRecord record : records) {
                 String schema = (String) record.getRecordMetaMap().get(RdbMetaKey.SCHEMA_NAME);
                 String table = (String) record.getRecordMetaMap().get(RdbMetaKey.TABLE_NAME);
-                String action = (String) record.getRecordMetaMap().get(RdbMetaKey.ACTION_NAME);
+                RecordAction action = (RecordAction) record.getRecordMetaMap().get(RdbMetaKey.ACTION_NAME);
 
-                if (schema.equals("wide_table") && table.equals("table_schedule")) {
+                if (schema.equals("wide_table") && table.equals("task_schedule")) {
                     CustomField pk;
-                    if (action.equals("INSERT") || action.equals("UPDATE")) {
-                        pk = record.getFieldMapAfter().get("worker_id");
-                    } else if (action.equals("DELETE")) {
-                        pk = record.getFieldMapBefore().get("worker_id");
-                    } else {
-                        continue;
+                    switch (action) {
+                        case INSERT:
+                        case UPDATE:
+                            pk = record.getFieldMapAfter().get("worker_id");
+                            break;
+                        case DELETE:
+                            pk = record.getFieldMapBefore().get("worker_id");
+                            break;
+                        default:
+                            continue;
                     }
 
-                    try (PreparedStatement ps = conn.prepareStatement("select * from worker where id=?")) {
+                    try (PreparedStatement ps = conn.prepareStatement("select * from wide_table.worker where id=?")) {
                         ps.setString(1, (String) pk.getValue());
                         ResultSet rs = ps.executeQuery();
                         while (rs.next()) {
@@ -53,7 +61,7 @@ public class OneFactTableOneDimensionTableProcessor implements CloudCanalProcess
                 }
             }
         } catch (Exception e) {
-            throw new RuntimeException("process error.");
+            throw new RuntimeException("process error.msg:" + ExceptionUtils.getRootCauseMessage(e), e);
         }
 
         return records;
