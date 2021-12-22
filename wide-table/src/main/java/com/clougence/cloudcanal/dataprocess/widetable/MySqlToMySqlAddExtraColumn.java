ea87b67558516305ba4db3970e1f8a481a31db90
zylicfc <zylicfc@192.168.0.171>
2021-12-22 18:16:43 +0800

    feat(code):add new field example

---
+++ wide-table/src/main/java/com/clougence/cloudcanal/dataprocess/widetable/MySqlToMySqlAddExtraColumn.java
@@ -0,0 +1,46 @@
+package com.clougence.cloudcanal.dataprocess.widetable;
+
+import java.sql.Types;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.clougence.cloudcanal.sdk.api.CloudCanalProcessorV2;
+import com.clougence.cloudcanal.sdk.api.ProcessorContext;
+import com.clougence.cloudcanal.sdk.api.modelv2.CustomData;
+import com.clougence.cloudcanal.sdk.api.modelv2.CustomFieldV2;
+import com.clougence.cloudcanal.sdk.api.modelv2.CustomRecordV2;
+import com.clougence.cloudcanal.sdk.api.modelv2.SchemaInfo;
+
+/**
+ * 纯粹打数据日志
+ * 
+ * @author bucketli 2021/11/29 23:07:26
+ */
+public class MySqlToMySqlAddExtraColumn implements CloudCanalProcessorV2 {
+
+    private SchemaInfo targetTable = new SchemaInfo(null, "dingtax", "worker_stats");
+
+    @Override
+    public void start(ProcessorContext context) {
+
+    }
+
+    @Override
+    public List<CustomData> process(CustomData data) {
+        List<CustomData> re = new ArrayList<>();
+        if (data.getSchemaInfo().equals(targetTable)) {
+            for (CustomRecordV2 recordV2 : data.getRecords()) {
+                recordV2.addField(CustomFieldV2.buildField("extra_col_bigint", 123, Types.BIGINT, false, false, true));
+                recordV2.addField(CustomFieldV2.buildField("extra_col_varchar", "added_str", Types.VARCHAR, false, false, true));
+            }
+        }
+
+        re.add(data);
+        return re;
+    }
+
+    @Override
+    public void stop() {
+        // do nothing
+    }
+}
