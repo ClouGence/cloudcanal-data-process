4ff51bd21162e8770c7ff3b4842dad530b79584c
zylicfc <zylicfc@zylicfcdeMacBook-Pro.local>
2022-03-30 21:20:52 +0800

    fix(ds):add clouddm data handle

---
+++ data-transform/src/main/java/com/clougence/clouddm/dataprocess/security/EncryptSensitiveData.java
@@ -0,0 +1,22 @@
+package com.clougence.clouddm.dataprocess.security;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.clougence.clouddm.sdk.api.ColMetaDataInSdk;
+import com.clougence.clouddm.sdk.api.ColProcess;
+
+/**
+ * @author bucketli 2022/3/30 19:35:53
+ */
+public class EncryptSensitiveData implements ColProcess {
+
+    @Override
+    public Object doProcess(Object colValue, ColMetaDataInSdk colMeta, String processConfig) {
+        if (colValue == null) {
+            return null;
+        }
+
+        String strVal = String.valueOf(colValue);
+        return StringUtils.repeat("$", strVal.length());
+    }
+}
